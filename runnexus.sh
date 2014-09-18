85c6cf506acf1b18f2d00e2efa3cee340d53da3f
andre <goroh@hotmail.com>
2014-09-18 13:59:13 -0300

    nginx host

--- runnexus.sh
+++ runnexus.sh
@@ -2,5 +2,6 @@ docker run --rm -t -i \
    -p 8081:8081 \
    -v /opt/sonatype-work:/opt/sonatype-work \
    -v /opt/nexus/conf:/opt/nexus/conf \
+   -e VIRTUAL_HOST=nexus.localdomain \
    andrefernandes/docker-nexus3:latest
 
